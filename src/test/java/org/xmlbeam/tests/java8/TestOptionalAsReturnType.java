288f365f6025311a2b3dd94f6ab1ecf75d18b7ce
SvenEwald <github@ewaelder.germantld>
2017-10-31 22:34:27 +0100

    Changed to most recent xmlbeam version

--- src/test/java/org/xmlbeam/tests/java8/TestOptionalAsReturnType.java
+++ src/test/java/org/xmlbeam/tests/java8/TestOptionalAsReturnType.java
@@ -1,6 +1,8 @@
 package org.xmlbeam.tests.java8;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Optional;
 
@@ -10,23 +12,34 @@ import org.xmlbeam.annotation.XBRead;
 
 public class TestOptionalAsReturnType {
 
-    public interface OptionalReturningProjection {
-        @XBRead("/foo/bar")
-        Optional<Integer> getSomeValue();
-    }
+	public interface OptionalReturningProjection {
+		@XBRead("/foo/bar")
+		Optional<Integer> getSomeValue();
 
-    @Test
-    public void testValueIsPresent() {
-        OptionalReturningProjection projection = new XBProjector().projectXMLString("<foo><bar>1</bar></foo>", OptionalReturningProjection.class);
-        Optional<Integer> value = projection.getSomeValue();
-        assertTrue(value.isPresent());
-        assertEquals(Integer.valueOf(1), value.get());
-    }
-    
-    @Test
-    public void testValueIsNotPresent() {
-        OptionalReturningProjection projection = new XBProjector().projectXMLString("<foo><bar2>2</bar2></foo>", OptionalReturningProjection.class);
-        Optional<Integer> value = projection.getSomeValue();
-        assertFalse(value.isPresent());
-    }
+		@XBRead("/foo/bar")
+		Optional<String> getSomeValueAsString();
+	}
+
+	@Test
+	public void testValueIsPresent() {
+		OptionalReturningProjection projection = new XBProjector().projectXMLString("<foo><bar>1</bar></foo>", OptionalReturningProjection.class);
+		Optional<Integer> value = projection.getSomeValue();
+		assertTrue(value.isPresent());
+		assertEquals(Integer.valueOf(1), value.get());
+	}
+
+	@Test
+	public void testValueIsNotPresent() {
+		OptionalReturningProjection projection = new XBProjector().projectXMLString("<foo><bar2>2</bar2></foo>", OptionalReturningProjection.class);
+		Optional<Integer> value = projection.getSomeValue();
+		assertFalse(value.isPresent());
+	}
+
+	@Test
+	public void testStringValueIsPresent() {
+		OptionalReturningProjection projection = new XBProjector().projectXMLString("<foo><bar>1</bar></foo>", OptionalReturningProjection.class);
+		Optional<String> value = projection.getSomeValueAsString();
+		assertTrue(value.isPresent());
+		assertEquals("1", value.get());
+	}
 }
