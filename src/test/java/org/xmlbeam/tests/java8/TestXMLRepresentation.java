ded08951233347a6c5fca838b4466566da3a60a7
SvenEwald <github@ewaelder.germantld>
2021-04-12 20:39:29 +0200

    only use java 8 features

--- src/test/java/org/xmlbeam/tests/java8/TestXMLRepresentation.java
+++ src/test/java/org/xmlbeam/tests/java8/TestXMLRepresentation.java
@@ -3,14 +3,16 @@ package org.xmlbeam.tests.java8;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.xmlbeam.XBProjector;
 import org.xmlbeam.dom.DOMAccess;
-import org.xmlbeam.util.intern.DOMHelper;
 
 public class TestXMLRepresentation {
 
@@ -22,9 +24,9 @@ public class TestXMLRepresentation {
 	public void createFile() throws IOException {
 		Path tempDirectory = Files.createTempDirectory("xbtest");
 		tempDirectory.toFile().deleteOnExit();
-		Path file = Files.createFile(tempDirectory.resolve(Path.of("testxml.xml")));
+		Path file = Files.createFile(tempDirectory.resolve(Paths.get("testxml.xml")));
 		file.toFile().deleteOnExit();
-		Files.writeString(file, XML);
+		Files.write(file, XML.getBytes(StandardCharsets.UTF_8));
 		XMLFileInput=file;
 		XMLFileOutput=tempDirectory.resolve("XMLoutput.xml");
 	}
@@ -36,7 +38,8 @@ public class TestXMLRepresentation {
 		DOMAccess read = projector.io().file(XMLFileInput.toFile()).read(DOMAccess.class);
 		//DOMHelper.trim(read.getDOMBaseElement());
 		projector.io().file(XMLFileOutput.toFile()).write(read);
-		assertEquals(Files.readString(XMLFileInput),Files.readString(XMLFileOutput));
+		
+		assertEquals(Files.readAllLines(XMLFileInput).stream().collect(Collectors.joining()),Files.readAllLines(XMLFileOutput).stream().collect(Collectors.joining()));
 		
 	}
 }
